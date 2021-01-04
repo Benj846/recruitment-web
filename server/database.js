c777cc5eafd0f8e7bac5d025e92fcca3a2f13fbb
teo <teo@genuinest.com>
2021-01-04 12:04:32 +0900

    database.js modifed

--- server/database.js
+++ server/database.js
@@ -1,12 +1,38 @@
 const mysql = require('mysql2');
 require('dotenv').config();
-const pool = mysql.createPool({
+let dbConfig = {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PW,
   database: process.env.DB_DATABASE
-});
+};
+
+// solution 1
+const pool = mysql.createPool(dbConfig);
 pool.query('select 1+1', (err, rows)=>{/* */});
+
+// // solution 2
+// let connection;
+// const handleDisconnect = () => {
+//   connection = mysql.createConnection(dbConfig);
+//   connection.connect(err => {
+//      if (err) {
+//        console.log('connecting db error', err);
+//        setTimeout(handleDisconnect, 2000);
+//      }
+//   });
+
+//   connection.on('error', err => {
+//     console.log('connection error', err);
+//     if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+//       handleDisconnect();
+//     } else {
+//       throw err;
+//     }
+//   })
+// }
+// handleDisconnect();
+
 // const connection = mysql.createConnection({
 //   host: process.env.DB_HOST,
 //   user: process.env.DB_USER,
