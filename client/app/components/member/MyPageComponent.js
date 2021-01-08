5cc18472cf570723835c45ebd8094ac14f33c6d6
teo <teo@genuinest.com>
2021-01-08 18:13:24 +0900

    마이페이지 진행 중(30%)

--- client/app/components/member/MyPageComponent.js
+++ client/app/components/member/MyPageComponent.js
@@ -1,10 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Select from 'react-select';
 import '../../styles/MyPageComponent';
 import david from './images/david.jpg';
 import closeBtn from './images/close_button.svg';
 
 function MyPageComponent() {
+
+    const API_HOST = "http://localhost:3333";
+    const MYPAGE_API_URL = `${API_HOST}/mypage`;
+    // const fetchMypage = () => {
+    //     fetch(`${MYPAGE_API_URL}`)
+    //         .then(res => res.json())
+    //         .then(json=>SVGMetadataElement(json))
+    // }
+    // useEffect(() => {
+    //     fetchMypage()
+    // }, []);
+
     const [memberInfo, setMemberInfo] = useState({
         name: 'david gandy',
         gender: 'male',
@@ -12,6 +24,7 @@ function MyPageComponent() {
         phone: '010-0000-0000',
         email: 'user@mail.com'
     });
+
     const [isPhoneChangeClicked, setPhoneChangeClicked] = useState(false);
     const toggleChangePhone = () => {
          const clicked = !isPhoneChangeClicked;
@@ -35,90 +48,194 @@ function MyPageComponent() {
         })
     }
 
+    const [isProfileClicked, setProfileClicked] = useState(true);
     const [isResumeClicked, setResumeClicked] = useState(false);
+    const [isNoticeClicked, setNoticeClicked] = useState(false);
+    const [isBookmarkClicked, setBookmarkClicked] = useState(false);
+    const [isAccountClicked, setAccountClicked] = useState(false);
+
+    const toggleProfileClick = () => {
+        if (isProfileClicked === true) {
+            return;
+        }
+        else {            
+            setProfileClicked(true);
+            setNoticeClicked(false);
+            setBookmarkClicked(false);
+            setAccountClicked(false);
+        }
+    }
+
+    const toggleNoticeClick = () => {
+        const flag = !isNoticeClicked;
+        setNoticeClicked(flag);
+    }
+
+    const [isOfferClicked, setOfferClicked] = useState(false);
+    const [isLikeCompanyClicked, setLikeCompanyClicked] = useState(false);
+    const [isLikeNoticeClicked, setLikeNoticeClicked] = useState(false);
+    const [isEtcClicked, setEtcClicked] = useState(false);
+
+
+    const toggleOfferClick = () => {
+        if (isOfferClicked === true) {
+            return;
+        } else {
+            setOfferClicked(true);
+            setLikeCompanyClicked(false);
+            setLikeNoticeClicked(false);
+            setEtcClicked(false);
+            setProfileClicked(false);
+        }
+    }
+
     const toggleResume = () => {
         const clicked = !isResumeClicked;
         setResumeClicked(clicked);
+    }
+
+    const toggleBookmarkClick = () => {
+        // if(isBookmarkClicked === true) {
+        //     return;
+        // } else {
+        //     setProfileClicked(false);
+        //     setNoticeClicked(false);
+        //     setBookmarkClicked(true);
+        //     setAccountClicked(false);
+        // }
+    }
+
+    const toggleAccountClick = () => {
+        if (isAccountClicked === true) {
+            return;
+        } else {
+            setProfileClicked(false);
+            setAccountClicked(true);
+            setNoticeClicked(false);            
+            setBookmarkClicked(false);
+        }
     }
     
     return(
         <div id="mypage-wrapper">
             <div className="mypage-skyscraper">
-                <div className="title">프로필/관심직무 관리</div>
-                <div className="notice">알림</div>
+                <div className="profile" onClick={toggleProfileClick}>프로필/관심직무 관리</div>
+                <div className="notice" onClick={toggleNoticeClick}>알림</div>
+                {isNoticeClicked ?
+                    <div className="notice-clicked-content">
+                        <div className="offer" onClick={toggleOfferClick}>채용제안</div>
+                        <div className="like-company">관심기업</div>
+                        <div className="like-notice">관심공고</div>
+                        <div className="etc">기타</div>
+                    </div>
+                    : null}
                 <div className="resume" onClick={toggleResume}>이력서관리</div>
                 {isResumeClicked ? 
-                    <div class="added-content">
+                    <div class="resume-clicked-content">
                         <div className="resume-list">이력서 목록</div>
                         <div className="edit-resume">이력서 작성</div>
                         <div className="career-list">경력기술서 목록</div>
                     </div>
                 : null}
-                <div className="book-mark">북마크</div>
-                <div className="account">계정관리</div>
+                <div className="book-mark" onClick={toggleBookmarkClick}>북마크</div>
+                <div className="account" onClick={toggleAccountClick}>계정관리</div>
             </div>
 
-            <div id="mypage-container">
-                <div id="mypage-content">
-                    <div className="profile-title">나의 프로필</div>
-                    <hr className="div-line"/>
-                    <div className="info">                    
-                        <div className="pic">
-                            <img src={david} width="160px" height="220px"></img>
-                            <div className="buttons">
-                                <button className="change-btn">변경</button>
-                                <button className="delete-btn">삭제</button>
-                            </div>                        
+            <div id="mypage-container">         
+                {/* 나의 프로필 */}
+                {isProfileClicked ?
+                <div id="profile-container">
+                    <div className="profile-content">
+                        <div className="profile-title">나의 프로필</div>
+                        <hr className="div-line"/>
+                        <div className="info">                    
+                            <div className="pic">
+                                <img src={david} width="160px" height="220px"></img>
+                                <div className="buttons">
+                                    <button className="change-btn">변경</button>
+                                    <button className="delete-btn">삭제</button>
+                                </div>                        
+                            </div>
+                            <div className="basic">
+                                <span className="name font-style">이름</span>
+                                <input className="input-name" defaultValue={memberInfo.name} disabled={true}/>
+                                <span className="gender font-style">성별</span>
+                                <button className="male">남</button>
+                                <button className="female">여</button>
+                                <span className="birth-title font-style">생년월일</span>
+                                <input className="birth" disabled={true} defaultValue={memberInfo.birth}/>
+                                <span className="phone-title font-style">전화번호</span>
+                                <input className="input-phone" defaultValue={memberInfo.phone}/>
+                                <button className="change-phone-btn" onClick={toggleChangePhone}>변경</button>
+                                <span className="email-title font-style">이메일</span>
+                                <input className="input-email" defaultValue={memberInfo.phone} disabled={true}/>
+                                <span className="job-status font-style">구직상태</span>
+                                <Select styles={customStyle} placeholder="선택해주세요" className="select-job-status"/>
+                                <hr className="basic-div"/>
+                                <div className="mailing-avail">
+                                    <span className="font-style">수신여부</span>
+                                    <div className="content">
+                                        <span className="font-style">마케팅/이벤트 관련 SMS/MMS 수신동의</span>
+                                        <button className="marketing-btn"></button>
+                                        <span className="font-style">패플라이 AI JOB 추천 메일 수신 여부</span>
+                                        <button className="job-offer-btn"></button>
+                                        <span className="font-style">공지/이벤트 메일 수신 여부</span>
+                                        <button className="noti-event-btn"></button>
+                                        <span className="font-style">관심기업 관련 메일 수신 여부</span>
+                                        <button className="like-btn"></button>
+                                    </div>
+                                </div>                        
+                            </div>
                         </div>
-                        <div className="basic">
-                            <span className="name font-style">이름</span>
-                            <input className="input-name" defaultValue={memberInfo.name} disabled={true}/>
-                            <span className="gender font-style">성별</span>
-                            <button className="male">남</button>
-                            <button className="female">여</button>
-                            <span className="birth-title font-style">생년월일</span>
-                            <input className="birth" disabled={true} defaultValue={memberInfo.birth}/>
-                            <span className="phone-title font-style">전화번호</span>
-                            <input className="input-phone" defaultValue={memberInfo.phone}/>
-                            <button className="change-phone-btn" onClick={toggleChangePhone}>변경</button>
-                            <span className="email-title font-style">이메일</span>
-                            <input className="input-email" defaultValue={memberInfo.phone} disabled={true}/>
-                            <span className="job-status font-style">구직상태</span>
-                            <Select styles={customStyle} placeholder="선택해주세요" className="select-job-status"/>
-                            <hr className="basic-div"/>
-                            <div className="mailing-avail">
-                                <span className="font-style">수신여부</span>
-                                <div className="content">
-                                    <span className="font-style">마케팅/이벤트 관련 SMS/MMS 수신동의</span>
-                                    <button className="marketing-btn"></button>
-                                    <span className="font-style">패플라이 AI JOB 추천 메일 수신 여부</span>
-                                    <button className="job-offer-btn"></button>
-                                    <span className="font-style">공지/이벤트 메일 수신 여부</span>
-                                    <button className="noti-event-btn"></button>
-                                    <span className="font-style">관심기업 관련 메일 수신 여부</span>
-                                    <button className="like-btn"></button>
-                                </div>
-                            </div>                        
-                        </div>
-                    </div>
-                    {isPhoneChangeClicked ? <ChangePhoneNumber closePopup={toggleChangePhone}/> : null }
-                    <div className="customize-job">
-                        <div className="header">
-                            <div className="title-content">
-                                <span className="title">내가 설정한 관심 직무</span>
-                                <span className="sub-title">세부 직무는 최대 6개까지 설정할 수 있습니다.</span>
-                            </div>                    
-                            <button className="submit">등록하기+</button>
-                        </div>
-                        <hr className="job-div"/>
-                        <div className="main">
-                            <div className="added-job">
-                                아직 등록된 관심직무가 없습니다.
-                            </div>                        
-                            <button className="save-btn">변경사항 저장</button>
+                        {isPhoneChangeClicked ? <ChangePhoneNumber closePopup={toggleChangePhone}/> : null }
+                        <div className="customize-job">
+                            <div className="header">
+                                <div className="title-content">
+                                    <span className="title">내가 설정한 관심 직무</span>
+                                    <span className="sub-title">세부 직무는 최대 6개까지 설정할 수 있습니다.</span>
+                                </div>                    
+                                <button className="submit">등록하기+</button>
+                            </div>
+                            <hr className="job-div"/>
+                            <div className="main">
+                                <div className="added-job">
+                                    아직 등록된 관심직무가 없습니다.
+                                </div>                        
+                                <button className="save-btn">변경사항 저장</button>
+                            </div>
                         </div>
                     </div>
                 </div>
+                : null}
+                {/* 나의 프로필 */}
+
+                {/* 채용 제안 */}
+                {isOfferClicked ? 
+                <div id="offer-container">
+                    <div id="offer-content">
+                        <div className="header">                            
+                            <div className="title">
+                                <span className="main">채용제안</span>
+                                <span className="sub">기업에서 보내온 채용 제안입니다.</span>
+                            </div>
+                            <div className="select-rows">
+                                <select>
+                                    <option>10개식 노출</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div className="notice-guide">
+                            알림을 확인할 수 있습니다. 알림은 3개월 후 삭제됩니다.
+                        </div>
+                        <table>
+                            <thead>
+
+                            </thead>
+                        </table>
+
+                    </div>
+                </div>
+                : null}
             </div>
         </div>
     );
