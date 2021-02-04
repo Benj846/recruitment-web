83555b05d99300f6db749f7b14c1a731674b8edd
Nine <nine@genuinest.com>
2021-02-04 17:44:06 +0900

    커리어 CSS 수정
    import career 폴더명 대소문자 수정, Resume.css의 content-title 수직 중앙 정렬, 커리어 CSS

--- client/app/components/Career/Career.js
+++ client/app/components/Career/Career.js
@@ -17,29 +17,29 @@ function Career() {
         <div className="career-status">
           <span>이력서 열람 요청</span>
         </div>
-        <div className="career-comp-name">(주)카카오커머스</div>
-        <div className="date">2021.01.06 13:00</div>
+        <span className="career-comp-name">(주)카카오커머스</span>
+        <span className="date">2021.01.06 13:00</span>
       </div>
-      <div className="vertical-line"></div>
       <div className="career-comp-logo">
         <img src={logoAuction} alt="logoAuction" className="logo" />
       </div>
-      <div className="vertical-line2"></div>
       <div className="span-wrapper">
         <div className="span-day-time">
-          답변 가능 기한이 3일 10시간 05분 남았습니다
+          <span>답변 가능 기한이 </span>
+          <span>3일 10시간 05분</span>
+          <span> 남았습니다</span>
         </div>
-        <div className="postname">서비스 직군 모집</div>
-        <div className="area-type">경기 성남시 | 정규직, 계약직</div>
+        <span className="postname">서비스 직군 모집</span>
+        <div className="recruit-info">
+          <span className="area-type">경기 성남시</span>
+          <span className="area-type">정규직, 계약직</span>
+        </div>
       </div>
       <div className="career-content-button">
-        <button
-          className="career-content-button"
-          onClick={() => setRequest(false)}
-        >
+        <button className="btn-confirm" onClick={() => setRequest(false)}>
           요청확인
         </button>
-        <button className="career-content-button">진행내역</button>
+        <button className="btn-history">진행내역</button>
       </div>
     </div>
   ));
@@ -60,11 +60,15 @@ function Career() {
         </div>
 
         <div className="career-span-container">
-          <div className="text">채용제안 관리</div>
+          <div className="text">
+            <span className="txt">총</span>
+            <span className="count">NN</span>
+            <span className="txt">개의 채용 제안을 받았습니다.</span>
+          </div>
           <span className="career-span-content-true">
-            최근1년간 받은 요청을 확인할 수 있습니다
+            내가 받은 채용 제안입니다.
           </span>
-          <div className="career-button-container">
+          {/* <div className="career-button-container">
             <button className="career-buttons">전체{all}건</button>
             <button className="career-buttons">
               이력서 열람요청 {received}건
@@ -73,13 +77,14 @@ function Career() {
               면접요청 {interviewRec}건
             </button>
             <button className="career-buttons">결과발표 {result}건</button>
-          </div>
+          </div> */}
           <div className="career-body">
             {proposal ? (
               <div className="plaintext">
-                채용제안이 아직 없네요. <br />
-                AI 추천 리스트를 보러가시겠어요?
-                <br />
+                <div className="txt">
+                  <span>채용제안이 아직 없네요.</span>
+                  <span>AI 추천 리스트를 보러가시겠어요?</span>
+                </div>
                 <button className="aiJob">AI Job 추천 보러가기</button>
               </div>
             ) : (
@@ -87,6 +92,7 @@ function Career() {
             )}
           </div>
         </div>
+        <div className="sky-scrapper">Sky scrapper</div>
       </div>
       <Footer />
     </div>
